d3c59f220a3fbb61a50fdbbae6f2cc8eaf3f7818
Yogesh Mundra <techyogimun@gmail.com>
2023-03-23 22:14:44 +0530

    loader authorization

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,17 @@ import Home from './pages/Home';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import './assets/global.css'
+import Loader from './components/Loader';
 import PublicRoute from './components/PublicRoute';
 import ProtectedRoute from './components/ProtectedRoute';
+import { useSelector } from 'react-redux';
+
 
 function App() {
+  const {loading}=useSelector(state=>state.alerts)
   return (
     <div>
+      {loading && <Loader />}
       <BrowserRouter>
       <Routes>
         <Route path='/' element={<ProtectedRoute><Home/></ProtectedRoute>}/>
--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -1,35 +1,38 @@
 import { message } from 'antd';
 import axios from 'axios';
 import React, {  useEffect, useState } from 'react'
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom'
 import { SetUser } from '../redux/usersSlice';
+import { HideLoading, ShowLoading } from "../redux/alertsSlice";
 
 function ProtectedRoute({children}) {
     const dispatch=useDispatch();
-    const [loading, setLoading] = useState(true);
+    const {loading}=useSelector((state)=>state.alerts);
     const navigate = useNavigate();
     const validateToken = async () => {
         try {
+            dispatch(ShowLoading());
             const response = await axios.post('/api/users/get-user-by-id', {}, {
                 headers: {
                     Authorization: `Bearer ${localStorage.getItem('token')}`
                 }
             })
+            dispatch(HideLoading())
             if (response.data.success) {
-                setLoading(false)
+                
                 dispatch(SetUser(response.data.data))
 
             }
             else {
-                setLoading(false)
+                
                 localStorage.removeItem("token");
                 message.error(response.data.message);
                 navigate('/login')
                 
             }
         } catch (error) {
-            setLoading(false)
+            dispatch(HideLoading())
             localStorage.removeItem("token");
             message.error(error.message);
             navigate('/login')
