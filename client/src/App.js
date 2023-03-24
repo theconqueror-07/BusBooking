c219c35ee246722d33f68434a0623dd112300a30
Yogesh Mundra <techyogimun@gmail.com>
2023-03-24 23:37:48 +0530

    layout structure

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,9 @@ import Loader from './components/Loader';
 import PublicRoute from './components/PublicRoute';
 import ProtectedRoute from './components/ProtectedRoute';
 import { useSelector } from 'react-redux';
+import AdminHome from './pages/Admin/AdminHome';
+import AdminBuses from './pages/Admin/AdminBuses';
+import AdminUsers from './pages/Admin/AdminUsers';
 
 
 function App() {
@@ -18,6 +21,11 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route path='/' element={<ProtectedRoute><Home/></ProtectedRoute>}/>
+        <Route path='/admin' element={<ProtectedRoute><AdminHome/></ProtectedRoute>}/>
+        <Route path='/admin/buses' element={<ProtectedRoute><AdminBuses/></ProtectedRoute>}/>
+        <Route path='/admin/users' element={<ProtectedRoute><AdminUsers/></ProtectedRoute>}/>
+        
+        
         <Route path='/register' element={<PublicRoute><Register /></PublicRoute>}/>
         <Route path='/login' element={<PublicRoute><Login /></PublicRoute>}/>
       </Routes>
