6fd022b34c0e683a8423dea2f046ba54e6ba6b79
Pratik <tripathypratik7@gmail.com>
2023-03-27 11:34:56 +0530

    bus interface layout

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const dbConfig=require("./config/dbConfig")
 const port= process.env.PORT || 5000;
 require('dotenv').config();
 app.use(express.json())
+
 const usersRoute = require('./routes/usersRoute')
+const busesRoute = require("./routes/busesRoute");
 
-
-
+app.use("/api/buses", busesRoute);
 app.use('/api/users',usersRoute)
 
 app.listen(port, ()=>console.log(`Server running on port ${port}`));
