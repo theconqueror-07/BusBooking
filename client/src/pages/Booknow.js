c60027141e00acf3217a3a1eb488fe13ef1f4a38
theconqueror-07 <tripathypratik7@gmail.com>
2023-03-28 08:51:36 +0530

    Payment Integration

--- client/src/pages/Booknow.js
+++ client/src/pages/Booknow.js
@@ -7,6 +7,14 @@ import { message,Col,Row } from 'antd';
 import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import SeatSelection from '../components/SeatSelection';
+import StripeCheckout from 'react-stripe-checkout';
+
+// Stripe:PaymentIntent.create(
+//   customer => customer.id,
+//   amount => params[amount],
+//   description => 'Rails Stripe transaction',
+//   currency => 'inr',
+// )
 
 
 function Booknow() {
@@ -31,12 +39,13 @@ function Booknow() {
         }
       };
 
-      const bookNow = async () => {
+      const bookNow = async (transactionId) => {
         try {
           dispatch(ShowLoading());
           const response = await axiosInstance.post("/api/bookings/book-seat", {
             bus: bus._id,
             seats: selectedSeats,
+            transactionId,
           });
           dispatch(HideLoading());
           if (response.data.success) {
@@ -51,10 +60,34 @@ function Booknow() {
         }
       };
 
+      const onToken=async(token)=>{
+        try{
+          dispatch(ShowLoading());
+          const response =await axiosInstance.post("/api/bookings/make-payment",{
+            token,
+            amount: selectedSeats.length * bus.fare * 100,
+          });
+          dispatch(HideLoading());
+          if(response.data.success)
+          {
+            message.success(response.data.message);
+            bookNow(response.data.data.transactionId);
+          }
+          else
+          {
+            message.error(response.data.message)
+          }
+        }catch (error){
+          dispatch(HideLoading());
+          message.error(error.message);
+        }
+      };
+
     useEffect
     (() => {
         getBus();
       }, []);
+
   return (
     <div>
     {bus && (<Row className='mt-3' gutter={20}>
@@ -79,9 +112,19 @@ function Booknow() {
             </h1> 
             <h1 className='text-2xl mt-2'>Price: Rs<b>{bus.fare*selectedSeats.length}</b></h1>
            </div>
-           <button className={`btn btn-primary ${
-            selectedSeats.length===0 && "disabled-btn"
-           }`} onClick={bookNow} disabled={selectedSeats.length===0}>Book Now</button>
+
+
+      <StripeCheckout
+      billingAddress
+        token={onToken}
+        amount={bus.fare * selectedSeats.length * 100}
+        currency="INR"
+        stripeKey="pk_test_51MqRR1SDx1AnvBM8yVwRLh2OTXOyGDdoq0TwV9UndHqiWU8XIN88iRLPOf4MrD9TpBsOoNdtEbdzJ0P9zRFacTJR00H0jUbrut">
+        <button className={`btn btn-primary ${
+          selectedSeats.length===0 && "disabled-btn"
+         }`} onClick={bookNow} disabled={selectedSeats.length===0}>Book Now</button>
+      </StripeCheckout>
+          
            
         </Col>
         <Col lg={12} xs={24} sm={24}>
--- routes/bookingsRoute.js
+++ routes/bookingsRoute.js
@@ -2,8 +2,8 @@ const router = require("express").Router();
 const authMiddleware = require("../middlewares/authMiddleware");
 const Booking = require("../models/bookingsModel");
 const Bus = require("../models/busModel");
-// const stripe = require("stripe")(process.env.stripe_key);
-// const { v4: uuidv4 } = require("uuid");
+const stripe = require("stripe")(process.env.stripe_key);
+const { v4: uuidv4 } = require('uuid');
 // book a seat
 
 router.post("/book-seat", authMiddleware, async (req, res) => {
@@ -32,49 +32,49 @@ router.post("/book-seat", authMiddleware, async (req, res) => {
 
 // make payment
 
-// router.post("/make-payment", authMiddleware, async (req, res) => {
-//   try {
-//     const { token, amount } = req.body;
-//     const customer = await stripe.customers.create({
-//       email: token.email,
-//       source: token.id,
-//     });
-//     const payment = await stripe.charges.create(
-//       {
-//         amount: amount,
-//         currency: "inr",
-//         customer: customer.id,
-//         receipt_email: token.email,
-//       },
-//       {
-//         idempotencyKey: uuidv4(),
-//       }
-//     );
+router.post("/make-payment", authMiddleware, async (req, res) => {
+  try {
+    const { token, amount } = req.body;
+    const customer = await stripe.customers.create({
+      email: token.email,
+      source: token.id,
+    });
+    const payment = await stripe.charges.create(
+      {
+        amount: amount,
+        currency: "inr",
+        customer: customer.id,
+        receipt_email: token.email,
+      },
+      {
+        idempotencyKey: uuidv4(),
+      }
+    );
 
-//     if (payment) {
-//       res.status(200).send({
-//         message: "Payment successful",
-//         data: {
-//           transactionId: payment.source.id,
-//         },
-//         success: true,
-//       });
-//     } else {
-//       res.status(500).send({
-//         message: "Payment failed",
-//         data: error,
-//         success: false,
-//       });
-//     }
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).send({
-//       message: "Payment failed",
-//       data: error,
-//       success: false,
-//     });
-//   }
-// });
+    if (payment) {
+      res.status(200).send({
+        message: "Payment successful",
+        data: {
+          transactionId: payment.source.id,
+        },
+        success: true,
+      });
+    } else {
+      res.status(500).send({
+        message: "Payment failed",
+        data: error,
+        success: false,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      message: "Payment failed",
+      data: error,
+      success: false,
+    });
+  }
+});
 
 // get bookings by user id
 router.post("/get-bookings-by-user-id", authMiddleware, async (req, res) => {
